6304c59862a7c919dbeb3225881419667f7c460e
HaoYi <pampas23@gmail.com>
2024-02-01 18:54:06 +0800

    use chatbot format

--- myapp.py
+++ myapp.py
@@ -1,20 +1,40 @@
-import time
+# Source:Below code is provided by Streamlit and AWS 
 
-import streamlit as st
+#1 import streamlit and chatbot file
+import streamlit as st 
+import chatbot_backend as demo  #**Import your Chatbot file as demo
 
-import numpy as np
-import pandas as pd
-st.title('我的第一個應用程式 Hello World')
-st.write("token: ")
-my_api_token = st.secrets["my_api_token"]
-st.write(my_api_token)
-st.write("嘗試創建**表格**：")
+#2 Set Title for Chatbot - https://docs.streamlit.io/library/api-reference/text/st.title
+st.title("Hi, This is Chatbot for Report :sunglasses:") # **Modify this based on the title you want in want
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-st.line_chart(chart_data)
+#3 LangChain memory to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
+if 'memory' not in st.session_state: 
+    # st.session_state.memory = demo.demo_memory() #** Modify the import and memory function() attributes initialize the memory
+    st.session_state.messages = []
 
-if st.button('Let it celebrate!'):
-    st.balloons()
+#4 Add the UI chat history to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
+if 'chat_history' not in st.session_state: #see if the chat history hasn't been created yet
+    st.session_state.chat_history = [] #initialize the chat history
 
+#5 Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
+for message in st.session_state.chat_history: 
+    with st.chat_message(message["role"]): 
+        st.markdown(message["text"]) 
+
+#6 Enter the details for chatbot input box 
+     
+input_text = st.chat_input("Powered by Bedrock and LLama 2") # **display a chat input box
+if input_text: 
+    
+    with st.chat_message("user"): 
+        st.markdown(input_text) 
+    
+    st.session_state.chat_history.append({"role":"user", "text":input_text}) 
+
+    # chat_response = demo.demo_conversation(input_text=input_text, memory=st.session_state.memory) #** replace with ConversationChain Method name - call the model through the supporting library
+    chat_response = f"Ok, {input_text}"
+    
+    with st.chat_message("assistant"): 
+        st.markdown(chat_response) 
+    
+    st.session_state.chat_history.append({"role":"assistant", "text":chat_response}) 
